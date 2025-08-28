23d0615b56b0184b9c681e3838a58725dbe72281
Michael Creel <michaelcreel815@gmail.com>
2025-08-28 14:52:46 -0600

    Memory Info
    Added getting memory info.

--- sysinfo.c
+++ sysinfo.c
@@ -4,10 +4,12 @@
 #include <limits.h>
 
 int main() {
+    //USER AND PROCESS INFORMATION
     printf("User ID: %d\n", getuid());
     printf("Group ID: %d\n", getgid());
     printf("Process ID: %d\n", getpid());
 
+    //CPU INFORMATION
     FILE *cpu = fopen("/proc/cpuinfo", "r");
     if (cpu) {
         printf("\n<--- CPU INFORMATION --->\n");
@@ -19,4 +21,19 @@ int main() {
         }
         fclose(cpu);
     }
+
+    //MEMORY INFORMATION
+    FILE *mem = fopen("/proc/meminfo", "r");
+    if (mem) {
+        printf("\n<--- MEMORY INFORMATION --->\n");
+        char line[256];
+        while (fgets(line, sizeof(line), mem)) {
+            if (strncmp(line, "MemTotal", 8) == 0 || strncmp(line, "MemFree", 7) == 0) {
+                printf("%s", line);
+            }
+        }
+        fclose(mem);
+    }
+
+    return 0;
 }
